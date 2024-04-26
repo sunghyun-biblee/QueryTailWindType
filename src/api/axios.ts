e107759a234931dd3810dee70dfc12ab4c190418
sunghyun1014 <sunghyun1014@naver.com>
2024-04-27 00:21:06 +0900

    typescript,reactQuery, tailwind  재설치

---
+++ src/api/axios.ts
@@ -0,0 +1,10 @@
+import axios from "axios";
+
+const instance = axios.create({
+  baseURL: "https://api.api-ninjas.com/v1/animals?",
+  headers: {
+    "X-Api-Key": "whRnoDG5P9gexc4FOYp4Tw==AxezDHyfYFfo5NxL",
+  },
+});
+
+export default instance;
---
+++ src/api/request.ts
@@ -0,0 +1,5 @@
+const request = {
+  dog: "name=dog",
+  cat: "name=cat",
+};
+export default request;
